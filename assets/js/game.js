b9d7d8e8ee885a3923858336c6f2cc57ec5939c5
AniElaina <anicook13@gmail.com>
2021-06-02 07:17:30 -0400

    updating robot gladiators module 3.1.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+//What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  //fight();
+
+  //This is string data type; it must be wrapped in double qutoes (" ") or single quotes (' ').
+  var stringDataType = "This is string, which is a fancy way to say text";
+
+  //This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
+  var numberIntegerDataType = 10;
+  var numberFloatDataType = 10.4;
+
+  // This is a Boolean data type, which can only be given a value of true or false.
+  var booleanDataType = true;
